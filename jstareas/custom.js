db792ff014da3fa8fa27ffe0e5ec96aa27368d4a
LuchoMunna <niko.and.luis@gmail.com>
2023-09-10 19:16:40 -0300

    3131

--- jstareas/custom.js
+++ jstareas/custom.js
@@ -1,36 +1,5 @@
-//console.log("Lucho");
-//console.log(5 + 5);
-//console.log( 5==6 );
-//document.write("holas");
-
-//const nombre="lucho";
-//const edad="26";
-
-//console.log(nombre + " " + edad);
-
-
-
-
-
-function horizontal(){
-    //console.log(document.getElementsByClassName("cajas")[0].classList) checkea clases
-    document.getElementsByClassName('cajas')[0].classList.add ("cajas2");
-    document.getElementsByClassName('cajas')[0].classList.remove("nocajas");
-    console.log(document.getElementsByClassName("cajas")[0].classList)
-}
-
-function vertical(){
-    
-    document.getElementsByClassName('cajas')[0].classList.remove("cajas2");
-    document.getElementsByClassName('cajas')[0].classList.remove("nocajas");
-    console.log(document.getElementsByClassName("cajas")[0].classList)
-} 
-
-function invisible(){
-    document.getElementsByClassName('cajas')[0].classList.add("nocajas")
-
-}
-
-
-
-var edad = prompt("Cuantos años tenes")
+// apis
+// http post para crear
+// get leer la iformacion
+// put  actualizar informacion
+// delete para borrar la iformacion
